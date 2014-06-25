0905fbfaeeab111ae19d99e169807f5c979f4088
Terrance Griffith <tjgriffi@ucsd.edu>
2014-06-25 12:07:12 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,13 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Terrance Griffith on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+@end
---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,13 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Terrance Griffith on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+@end
