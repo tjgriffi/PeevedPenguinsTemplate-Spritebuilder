94dcfcf502f69269683d0a66e798430c03a0c474
Terrance Griffith <tjgriffi@ucsd.edu>
2014-06-25 23:25:04 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -40,14 +40,14 @@
         CGPoint touchLocation = [touch locationInNode:_contentNode];
         
         // start catapult dragging when a touch inside of the catapult arm occurs
-        if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
-        {
+        //if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
+        //{
             // move the mouseJointNode to the touch position
-            _mouseJointNode.position = touchLocation;
+          //  _mouseJointNode.position = touchLocation;
             
             // setup a spring joint between the mouseJointNode and the catapultArm
-            _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
-        }
+            //_mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+       /// }
     }
 
 - (void)launchPenguin {
